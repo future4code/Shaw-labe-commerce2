ace88a7c3b8ea224bf39d280edbf2cf3255071bd
William <william-freitas18@hotmail.com>
2022-03-31 19:37:24 -0300

    Estilização inicial de cards

--- src/Componentes/CardProduto.js
+++ src/Componentes/CardProduto.js
@@ -19,18 +19,37 @@ height: 60vh;
 margin-left: 0.2%; 
 margin-right: 0.2%; 
 margin-top: 2%; 
+box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
 `
 
-
+const FundoImagemCartao = styled.div`
+background-color: #dadfe8;
+width: 100%;
+height: 60%;
+`
 
 const ImagemCartao = styled.img`
+display: block;   
 width: 40%;
-height: 60%;
-align-self: center;
+height: 100%;
+margin-left: auto;   
+margin-right: auto;
 `
 
 const BotaoCartao = styled.button`
-width: 50%;
+width: 40%;
+height: 25%;
+background-color: #FFFFFF;
+color: black;
+`
+
+const ConteudoCartao = styled.div`
+width: 100%;
+height: 40%;
+background-color: #272134;
+text-align: center;
+justify-content: center;
+color: #bf870d;
 `
 
 
@@ -46,10 +65,17 @@ export default class CardProduto extends React.Component {
         let produto = this.props.Produto
         return (
             <ContainerCard>
-                <ImagemCartao img src={imgArmored} />
-                <p>{produto.name}</p>
-                <p>R$ {produto.price}</p>
-                <button>Adicionar ao Carrinho</button>
+
+                <FundoImagemCartao>
+                    <ImagemCartao img src={imgArmored} />
+                </FundoImagemCartao>
+
+                <ConteudoCartao>
+                    <p>{produto.name}</p>
+                    <p>R$ {produto.price}</p>
+                    <BotaoCartao button>Adicionar ao Carrinho</BotaoCartao>
+                </ConteudoCartao>
+
             </ContainerCard>
         )
     }
